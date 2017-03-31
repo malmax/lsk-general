fa3844d3d8c31f89d26161b6ba7d6f5b01b528a8
malmax.spb@gmail.comm <malmax.spb@gmail.com>
2017-03-31 23:56:43 +0300

    complete task

--- src/General/Avatar/Avatar.jsx
+++ src/General/Avatar/Avatar.jsx
@@ -1,8 +1,8 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import importcss from 'importcss';
 import cx from 'classnames';
-import { Image, Col, Row, Grid } from 'react-bootstrap';
-import Media from 'react-media';
+import {Image, Col, Row, Grid} from 'react-bootstrap';
+import MediaQuery from 'react-responsive';
 
 @importcss(require('./Avatar.css'))
 export default class Avatar extends Component {
@@ -13,25 +13,38 @@ export default class Avatar extends Component {
     placeholder: PropTypes.string,
     alt: PropTypes.string,
     title: PropTypes.string,
+    border: PropTypes.bool,
+    borderActive: PropTypes.bool,
+    grayFilter: PropTypes.bool,
+    shadow: PropTypes.bool,
+    borderCircle: PropTypes.bool
   }
 
   static defaultProps = {
     placeholder: 'defaultAvatar.jpg',
-    alt: 'Аватар пользователя',
-    title: 'Пользователь',
+    border: false,
+    borderActive: false,
+    grayFilter: false,
+    shadow: false,
+    borderCircle: false
   }
 
   constructor(props) {
     super();
 
+    // дефолтные пропсы в зависимости от других пропсов
+    this.altText = props.alt || `Аватар пользователя ${props.username}`;
+    this.titleText = props.title || `Пользователь ${props.username}`;
+    const border = props.border || props.borderActive;
+
     // вначале ставим заглушку - placeholder
     this.state = {
-      avatarSrc: this.requireImage(props.placeholder),
+      avatarSrc: this.requireImage(props.placeholder)
     };
 
     // Если есть изображение высокой четкости
-    if (props.avatarHighSrc) {
-      this.avatarHighSrc = this.requireImage(props.avatarHighSrc);
+    if (props.imgHighRes) {
+      this.avatarHighSrc = this.requireImage(props.imgHighRes);
     } else {
       this.avatarHighSrc = this.requireImage(props.img);
     }
@@ -41,16 +54,26 @@ export default class Avatar extends Component {
     if (props.badge) {
       this.badge = this.requireImage(props.badge);
     }
+
+    // Стили
+    this.avatarStyles = cx({
+      'Avatar_image': true,
+      'Avatar_border_active': props.borderActive,
+      'Avatar_border': border,
+      'Gray_Filter': props.grayFilter,
+      'Border_circle': props.borderCircle
+    });
+    console.log(props.border, this.avatarStyles);
   }
 
   componentDidMount() {
     new Promise((resolve) => {
       if (this.props.img) {
         const avatarSrc = this.requireImage(this.props.img);
-
-        setTimeout(resolve.bind(this, { avatarSrc }), 2000);
+        // TODO: del setTimeout on production
+        setTimeout(resolve.bind(this, {avatarSrc}), 1000);
       }
-    }).then(avatarSrc => this.setState(avatarSrc, () => console.log(this.state)));
+    }).then(avatarSrc => this.setState(avatarSrc));
   }
 
   requireImage(imgPath) {
@@ -69,25 +92,28 @@ export default class Avatar extends Component {
   }
 
   render() {
-    const avatarStyles = cx(['Avatar_image'] : true,);
 
     return (
       <Grid>
         <Row>
           <Col xs={12} md={4}>
-            <Media query="(max-width: 1500px)">
-              {matches => matches
-                ? (
-                  <Image responsive src={this.state.avatarSrc} alt={this.props.alt} title={this.props.title} styleName={avatarStyles} />
-                )
-                : (
-                  <Image responsive src={this.avatarHighSrc} alt={this.props.alt} title={this.props.title} styleName={avatarStyles} />
-                )}
-            </Media>
+            <div styleName={cx({
+              'Avatar_container': true,
+              'Shadow': this.props.shadow,
+              })}>
+              <MediaQuery query='(max-width: 1224px)'>
+                <Image responsive src={this.state.avatarSrc} alt={this.altText} title={this.titleText} styleName={this.avatarStyles}/>
+              </MediaQuery>
 
-            <If condition={this.badge}>
-              <Image src={this.badge} styleName="Avatar_badge" />
-            </If>
+              <MediaQuery query='(min-width: 1224px)'>
+                <Image responsive src={this.avatarHighSrc} alt={this.altText} title={this.titleText} styleName={this.avatarStyles}/>
+
+              </MediaQuery>
+
+              <If condition={this.badge}>
+                <Image src={this.badge} styleName="Avatar_badge"/>
+              </If>
+            </div>
           </Col>
         </Row>
       </Grid>
--- src/General/Avatar/Avatar.story.jsx
+++ src/General/Avatar/Avatar.story.jsx
@@ -33,7 +33,7 @@ module.exports = function ({ storiesOf, action }) {
           img={'myavatar.jpg'}
           username="avatar"
           placeholder="loading.gif"
-          badge="http://www.iconsdb.com/icons/preview/orange/new-badge-3-xxl.png"
+          badge="badge.png"
         />
       </div>
     ))
@@ -43,7 +43,56 @@ module.exports = function ({ storiesOf, action }) {
           img={'myavatar.jpg'}
           username="avatar"
           placeholder="loading.gif"
-          avatarHighSrc="http://www.placehold.it/1000?text=High+Resolution+Image"
+          imgHighRes="http://www.placehold.it/300?text=High+Resolution+Image"
+        />
+      </div>
+    ))
+    .add('border', () => (
+      <div>
+        <Avatar
+          img={'myavatar.jpg'}
+          username="avatar"
+          placeholder="loading.gif"
+          border
+        />
+      </div>
+    ))
+    .add('border active', () => (
+      <div>
+        <Avatar
+          img={'myavatar.jpg'}
+          username="avatar"
+          borderActive
+        />
+      </div>
+    ))
+    .add('border circle', () => (
+      <div>
+        <Avatar
+          img={'myavatar.jpg'}
+          username="avatar"
+          borderActive
+          borderCircle
+        />
+      </div>
+    ))
+    .add('gray filter + border', () => (
+      <div>
+        <Avatar
+          img={'myavatar.jpg'}
+          username="avatar"
+          placeholder="loading.gif"
+          border
+          grayFilter
+        />
+      </div>
+    ))
+    .add('shadow', () => (
+      <div>
+        <Avatar
+          img={'myavatar.jpg'}
+          username="avatar"
+          shadow
         />
       </div>
     ));
